54bae5aa45089aa05cdcfd163029ca02955a1f0c
Nikhil Raj <131578072+NikhilRajOfficial@users.noreply.github.com>
2024-08-24 17:34:05 +0530

    Add files via upload

---
+++ src/test/java/com/nikhil/RecaptchaValidation/RecaptchaValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.RecaptchaValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecaptchaValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
